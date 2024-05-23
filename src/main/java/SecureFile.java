4dd222979f8f9f950ff7dc0c6d0bf11fbc675f5e
m.duemel@gmx.de <maurice.duemmel@gmail.com>
2024-05-23 12:21:01 +0200

    using alt version of SecureFile

--- src/main/java/SecureFile.java
+++ src/main/java/SecureFile.java
@@ -1,80 +1,101 @@
-import java.io.*;
-import java.nio.file.*;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Arrays;
 
 public class SecureFile {
-    private TripleDES tripleDES;
-    private byte[] iv;
+    private TripleDES des;
+    private byte[] init_vector = new byte[8];
+    private String operation;
 
-    public SecureFile(byte[] key, byte[] iv) {
-        if (key.length != 24 || iv.length != 8) {
-            throw new IllegalArgumentException("Invalid key or IV length");
+    public SecureFile(String key_file, String _operation){
+        try {
+            readKey(key_file);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        byte[] keyPart1 = Arrays.copyOfRange(key, 0, 8);
-        byte[] keyPart2 = Arrays.copyOfRange(key, 8, 16);
-        byte[] keyPart3 = Arrays.copyOfRange(key, 16, 24);
-        this.tripleDES = new TripleDES(keyPart1, keyPart2, keyPart3);
-        this.iv = iv;
+        operation = _operation;
     }
 
-    public void processFile(String inputFilePath, String outputFilePath, String operation) throws IOException {
-        byte[] inputBytes = Files.readAllBytes(Paths.get(inputFilePath));
-        byte[] outputBytes = new byte[inputBytes.length];
+    private void readKey(String key_file) throws IOException {
+        FileInputStream key_stream = new FileInputStream(key_file);
+        System.out.println("Key file: " + key_file);
 
-        byte[] feedback = iv.clone();
+        byte[] key_bytes;
+        key_bytes = key_stream.readAllBytes();
+        System.out.println("Key bytes: " + key_bytes.length);
 
-        for (int i = 0; i < inputBytes.length; i += 8) {
-            byte[] block = Arrays.copyOfRange(inputBytes, i, Math.min(i + 8, inputBytes.length));
-            byte[] encryptedFeedback = tripleDES.encryptBytes(feedback);
+        if(key_bytes.length != 32){
+            throw new IOException("Key file must contain 32 bytes.");
+        }
 
-            if (operation.equalsIgnoreCase("encrypt")) {
-                byte[] ciphertextBlock = xor(block, encryptedFeedback);
-                System.arraycopy(ciphertextBlock, 0, outputBytes, i, ciphertextBlock.length);
-                feedback = ciphertextBlock;
-            } else if (operation.equalsIgnoreCase("decrypt")) {
-                byte[] plaintextBlock = xor(block, encryptedFeedback);
-                System.arraycopy(plaintextBlock, 0, outputBytes, i, plaintextBlock.length);
+        byte[] key1 = new byte[8];
+        byte[] key2 = new byte[8];
+        byte[] key3 = new byte[8];
+
+        System.arraycopy(key_bytes, 0, key1, 0, 8);
+        System.arraycopy(key_bytes, 8, key2, 0, 8);
+        System.arraycopy(key_bytes, 16, key3, 0, 8);
+        System.arraycopy(key_bytes, 24, init_vector, 0, 8);
+
+
+        des = new TripleDES(key1, key2, key3);
+    }
+
+    public void processFile(String inputFilePath, String outputFilePath) throws IOException {
+
+        FileInputStream in = new FileInputStream(inputFilePath);
+        FileOutputStream out = new FileOutputStream(outputFilePath);
+        byte[] buffer = new byte[8];
+        int len;
+        byte[] feedback = init_vector.clone();
+
+        while((len = in.read(buffer)) > 0) {
+            byte[] block = buffer.clone();
+            if (len < 8) {
+                block = Arrays.copyOf(block, 8);
+            }
+            byte[] encrypted_feedback = des.encryptBytes(feedback);
+
+            if (operation.equals("encrypt")) {
+                byte[] cyber_block = myxor(block, encrypted_feedback);
+                out.write(cyber_block, 0, len);
+                feedback = cyber_block;
+            }
+            else if (operation.equals("decrypt")){
+                byte[] plain_block = myxor(block, encrypted_feedback);
+                out.write(plain_block, 0, len);
                 feedback = block;
-            } else {
-                throw new IllegalArgumentException("Invalid operation: " + operation);
+            }
+            else{
+                throw new IOException("Invalid operation: " + operation);
             }
         }
+        in.close();
+        out.close();
 
-        Files.write(Paths.get(outputFilePath), outputBytes);
     }
 
-    private byte[] xor(byte[] a, byte[] b) {
+    private byte[] myxor (byte[] a, byte[] b){
         byte[] result = new byte[a.length];
-        for (int i = 0; i < a.length; i++) {
+        for (int i = 0; i < a.length; i++){
             result[i] = (byte) (a[i] ^ b[i]);
         }
         return result;
     }
-
-    public static void main(String[] args) {
-        if (args.length != 4) {
-            System.err.println("Usage: java SecureFile <inputFile> <keyFile> <outputFile> <encrypt|decrypt>");
+    public static void main(String[] args) throws IOException {
+        if(args.length < 4){
+            System.err.printf("Not enough arguments. %d arguments given, 4 arguments needed.%n", args.length);
             return;
         }
 
-        String inputFile = args[0];
-        String keyFile = args[1];
-        String outputFile = args[2];
+        String input_file = args[0];
+        String key_file = args[1];
+        String output_file = args[2];
         String operation = args[3];
 
-        try {
-            byte[] keyAndIv = Files.readAllBytes(Paths.get(keyFile));
-            if (keyAndIv.length != 32) {
-                throw new IllegalArgumentException("Key file must be exactly 32 bytes long");
-            }
-            byte[] key = Arrays.copyOfRange(keyAndIv, 0, 24);
-            byte[] iv = Arrays.copyOfRange(keyAndIv, 24, 32);
 
-            SecureFile secureFile = new SecureFile(key, iv);
-            secureFile.processFile(inputFile, outputFile, operation);
-            System.out.println(operation + "ion completed successfully.");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        SecureFile sf = new SecureFile(key_file, operation);
+        sf.processFile(input_file, output_file);
     }
 }
